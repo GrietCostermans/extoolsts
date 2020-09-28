5a1d3a149635b3be242c2316b2a4f719c24cd41b
GrietCostermans <grietje.costermans@bnpparibasfortis.com>
2020-09-28 10:39:06 +0200

    tweede poging

---
+++ HelloWorld2/src/be/abis/helloworld2/test/HelloWorld2.java
@@ -0,0 +1,10 @@
+package be.abis.helloworld2.test;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Griet tweede poging");
+	}
+
+}
